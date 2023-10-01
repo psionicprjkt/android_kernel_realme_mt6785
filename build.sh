ccadc4475135c09d6222d1ae2fd6374e3e785def
officialputuid <officialputuid@hack.id>
2023-10-01 07:10:35 +0000

    treewide: Added build scripts
    Signed-off-by: officialputuid <officialputuid@hack.id>

---
+++ build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+function psionic_compile()
+{
+
+source ~/.bashrc && source ~/.profile
+export LC_ALL=C && export USE_CCACHE=1
+ccache -M 100G
+export ARCH=arm64
+
+git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang.git clang
+
+[ -d "out" ] && rm -rf AnyKernel && rm -rf out || mkdir -p out
+
+make O=out ARCH=arm64 RM6785_defconfig
+
+PATH="${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC="clang" \
+                      LD=ld.lld \
+		      AR=llvm-ar \
+		      NM=llvm-nm \
+		      OBJCOPY=llvm-objcopy \
+		      OBJDUMP=llvm-objdump \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE="${PWD}/clang/bin/aarch64-linux-gnu-" \
+                      CROSS_COMPILE_ARM32="${PWD}/clang/bin/arm-linux-gnueabi-" \
+                      CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log
+}
+
+function psionic_upload()
+{
+rm -rf AK3* && rm -rf AnyKernel
+wget https://psionicprjkt.my.id/assets/files/AK3-RM6785.zip && unzip AK3-RM6785
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel && cd AnyKernel
+date=$(date "+%d%m%Y")
+zip -r9 psionicKSU-RM6785-$date-TEST.zip *
+wget https://raw.githubusercontent.com/Sushrut1101/GoFile-Upload/master/upload.sh
+. upload.sh psionicKSU-RM6785-$date-TEST.zip
+}
+
+psionic_compile
+psionic_upload
