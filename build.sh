7a5ed1e0cc6013815626c5b63fe9f60b84b652d0
officialputuid <officialputuid@hack.id>
2023-12-13 23:29:46 +0000

    wf: Update builds kernel
    Signed-off-by: officialputuid <officialputuid@hack.id>

--- build.sh
+++ build.sh
@@ -8,9 +8,11 @@ function psionic_compile()
     source ~/.bashrc
     source ~/.profile
 
-    # download_clang
+    # download_resources
     wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r498229b.tar.gz -O "aosp-clang.tar.gz"
     mkdir clang && tar -xf aosp-clang.tar.gz -C clang && rm -rf aosp-clang.tar.gz
+    git clone --depth=1 https://github.com/psionicprjkt/aarch64-linux-android-4.9 arm64
+    git clone --depth=1 https://github.com/psionicprjkt/arm-linux-androideabi-4.9 arm32
 
     # cleanup_directories
     rm -rf out AnyKernel AK3-* && mkdir -p out
@@ -18,8 +20,10 @@ function psionic_compile()
     # compile_kernel
     export ARCH=arm64
     make O=out ARCH=arm64 RM6785_defconfig
-    PATH="${PWD}/clang/bin:${PATH}" \
+    PATH="${PWD}/clang/bin:${PWD}/arm64:${PWD}/arm32:${PATH}" \
         make -j$(nproc --all) O=out \
+        LLVM=1 \
+        LLVM_IAS=1 \
         ARCH=arm64 \
         CC="clang" \
         LD=ld.lld \
@@ -30,8 +34,8 @@ function psionic_compile()
         OBJCOPY=llvm-objcopy \
         OBJDUMP=llvm-objdump \
         CLANG_TRIPLE=aarch64-linux-gnu- \
-        CROSS_COMPILE="${PWD}/clang/bin/aarch64-linux-gnu-" \
-        CROSS_COMPILE_ARM32="${PWD}/clang/bin/arm-linux-gnueabi-" \
+        CROSS_COMPILE="${PWD}/arm64/aarch64-linux-android-" \
+        CROSS_COMPILE_COMPAT="${PWD}/arm32/arm-linux-androideabi-" \
         CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log
 }
 
