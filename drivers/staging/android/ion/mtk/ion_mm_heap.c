9063d6b01be0619180b2a5a3da98278aff16e983
officialputuid <officialputuid@hack.id>
2024-01-01 12:23:28 +0000

    drivers/staging/android/ion/mtk/ion_mm_heap.c: Drop unused variable buffer_info & mark ion_mm_heap_init_domain() as __maybe_unused
    ../drivers/staging/android/ion/mtk/ion_mm_heap.c:2451:31: warning: unused variable 'buffer_info' [-Wunused-variable]
     2451 |                         struct ion_mm_buffer_info *buffer_info =
          |                                                    ^~~~~~~~~~~
    ../drivers/staging/android/ion/mtk/ion_mm_heap.c:311:12: warning: unused function 'ion_mm_heap_init_domain' [-Wunused-function]
      311 | static int ion_mm_heap_init_domain(struct ion_mm_buffer_info *buffer_info,
          |            ^~~~~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: officialputuid <officialputuid@hack.id>

--- drivers/staging/android/ion/mtk/ion_mm_heap.c
+++ drivers/staging/android/ion/mtk/ion_mm_heap.c
@@ -308,7 +308,7 @@ int ion_get_domain_id(int from_kernel, int *port)
 static int ion_mm_heap_phys(struct ion_heap *heap, struct ion_buffer *buffer,
 			    ion_phys_addr_t *addr, size_t *len);
 
-static int ion_mm_heap_init_domain(struct ion_mm_buffer_info *buffer_info,
+static int __maybe_unused ion_mm_heap_init_domain(struct ion_mm_buffer_info *buffer_info,
 				   unsigned int domain)
 {
 #if defined(CONFIG_MTK_IOMMU_PGTABLE_EXT) && \
@@ -2448,8 +2448,6 @@ long ion_mm_ioctl(struct ion_client *client, unsigned int cmd,
 		}
 
 		if ((int)buffer->heap->type == ION_HEAP_TYPE_MULTIMEDIA) {
-			struct ion_mm_buffer_info *buffer_info =
-			    buffer->priv_virt;
 			enum ION_MM_CMDS mm_cmd = param.mm_cmd;
 			ion_phys_addr_t phy_addr;
 
